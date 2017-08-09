37b29263ab55821fed3db61d827c2fb52323c359
Jedi <hmgaar@gmail.com>
2017-08-08 21:40:50 -0400

    added DetoxComponent to our HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+// import {DetoxComponent} from '../../app/components/detox/detox.component'
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  template: `
+  <h1>Home</h1>
+
+<!--  <ion-header>
+    <ion-navbar>
+      <ion-title>Home</ion-title>
+    </ion-navbar>
+  </ion-header>-->
+
+  <ion-content padding>
+    <detox-component></detox-component>
+  </ion-content>
+  `
 })
 export class HomePage {
 
